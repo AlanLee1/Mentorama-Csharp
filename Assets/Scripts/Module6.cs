bd9d305f920aea24a7d7f023a923e692b5877970
alan.lee <alan.lee@pti.org.br>
2022-06-28 14:03:41 -0300

    fix: image module 2

--- Assets/Scripts/Module6.cs
+++ Assets/Scripts/Module6.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Module6 : MonoBehaviour
@@ -7,14 +8,17 @@ public class Module6 : MonoBehaviour
     enum EnemyState
     {
         Stopped,
-        Patrolling1,
-        Patrolling2
+        Patrolling
+        //Patrolling1,
+        //Patrolling2
     }
 
     enum PatrolDirection
     {
         Left,
-        Right
+        Right,
+        Top,
+        Bottom
     }
 
     [Serializable]
@@ -23,15 +27,18 @@ public class Module6 : MonoBehaviour
         public float PatrolDuration;
         public float MoveSpeed;
         public float MoveDirectionDuration;
+        public float StayDuration;
     }
 
     //Variaveis
     [SerializeField] EnemyState currentEnemyState;
-    [SerializeField] PatrolData patrolData1;
-    [SerializeField] PatrolData patrolData2;
+    //[SerializeField] PatrolData patrolData1;
+    //[SerializeField] PatrolData patrolData2;
+    [SerializeField] List<PatrolData> listPatrolData = new List<PatrolData>();
     [SerializeField] PatrolDirection currentPatrolDirection;
     private float directionChangeTime;
     private float startpatrolTime;
+    private int typePatrol;
 
 
     //Inicializando
@@ -40,10 +47,23 @@ public class Module6 : MonoBehaviour
         currentEnemyState = EnemyState.Stopped;
         currentPatrolDirection = PatrolDirection.Right;
         directionChangeTime = 0;
+        typePatrol = 0;
+        listPatrolData.Add(new PatrolData() { PatrolDuration = 8, MoveSpeed = 1, MoveDirectionDuration = 1.5f, StayDuration = 0.25f });
+        listPatrolData.Add(new PatrolData() { PatrolDuration = 12, MoveSpeed = 2, MoveDirectionDuration = 2f, StayDuration = 0.5f });
+        listPatrolData.Add(new PatrolData() { PatrolDuration = 12, MoveSpeed = 3, MoveDirectionDuration = 2.5f, StayDuration = 0.25f });
+        listPatrolData.Add(new PatrolData() { PatrolDuration = 16, MoveSpeed = 4, MoveDirectionDuration = 3f, StayDuration = 0.5f });
     }
 
     //Cada frame
     void Update()
+    {
+
+        AlterRotine();
+        ChangeEnemyState();
+
+    }
+
+    private void ChangeEnemyState()
     {
         //Estado: patrulhando ou parado
         switch (currentEnemyState)
@@ -52,35 +72,67 @@ public class Module6 : MonoBehaviour
             case EnemyState.Stopped:
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
-                    currentEnemyState = EnemyState.Patrolling1;
+                    currentEnemyState = EnemyState.Patrolling;
                     startpatrolTime = Time.time;
                 }
                 break;
 
-            case EnemyState.Patrolling1:
-                if (Time.time > startpatrolTime + patrolData1.PatrolDuration)
-                {
-                    currentEnemyState = EnemyState.Patrolling2;
-                    startpatrolTime = Time.time;
-                } else
-                {
-                    PatrolRotine(patrolData1);
-                }
-                break;
-            
-            case EnemyState.Patrolling2:
-                if (Time.time > startpatrolTime + patrolData2.PatrolDuration)
+            case EnemyState.Patrolling:
+
+                if (Time.time > startpatrolTime + listPatrolData[typePatrol].PatrolDuration)
                 {
                     currentEnemyState = EnemyState.Stopped;
+                    startpatrolTime = Time.time;
                 } else
                 {
-                    PatrolRotine(patrolData2);
+                    PatrolRotine(listPatrolData[typePatrol]);
                 }
                 break;
-        }
 
+
+                /*
+                case EnemyState.Patrolling1:
+                    if (Time.time > startpatrolTime + patrolData1.PatrolDuration)
+                    {
+                        currentEnemyState = EnemyState.Patrolling2;
+                        startpatrolTime = Time.time;
+                    } else
+                    {
+                        PatrolRotine(patrolData1);
+                    }
+                    break;
+
+                case EnemyState.Patrolling2:
+                    if (Time.time > startpatrolTime + patrolData2.PatrolDuration)
+                    {
+                        currentEnemyState = EnemyState.Stopped;
+                    } else
+                    {
+                        PatrolRotine(patrolData2);
+                    }
+                    break;
+                */
+        }
     }
 
+    private void AlterRotine()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (typePatrol == 3)
+            {
+                typePatrol = 0;
+                Debug.Log($"typePatrol: {typePatrol}");
+            } else
+            {
+                typePatrol++;
+                Debug.Log($"typePatrol: {typePatrol}");
+                typePatrol = Mathf.Clamp(typePatrol, 0, 4);
+            }
+        }
+    }
+
+    //Patrol Rotine
     private void PatrolRotine(PatrolData patrolData)
     {
         directionChangeTime += Time.deltaTime;
@@ -89,18 +141,29 @@ public class Module6 : MonoBehaviour
         switch (currentPatrolDirection)
         {
             case PatrolDirection.Right:
-                ChangePatrolDirection(patrolData, PatrolDirection.Left);
+                ChangePatrolDirection(patrolData, PatrolDirection.Top);
                 //patrol move
                 Translate(new Vector3(patrolData.MoveSpeed * Time.deltaTime, 0, 0));
+                break;
 
-
+            case PatrolDirection.Top:
+                ChangePatrolDirection(patrolData, PatrolDirection.Left);
+                //patrol move
+                Translate(new Vector3(0, 0, patrolData.MoveSpeed * Time.deltaTime));
                 break;
 
 
             case PatrolDirection.Left:
-                ChangePatrolDirection(patrolData, PatrolDirection.Right);
+                ChangePatrolDirection(patrolData, PatrolDirection.Bottom);
                 //patrol move
                 Translate(new Vector3(-patrolData.MoveSpeed * Time.deltaTime, 0, 0));
+                break;
+
+
+            case PatrolDirection.Bottom:
+                ChangePatrolDirection(patrolData, PatrolDirection.Right);
+                //patrol move
+                Translate(new Vector3(0, 0, -patrolData.MoveSpeed * Time.deltaTime));
                 break;
         }
     }
@@ -108,6 +171,7 @@ public class Module6 : MonoBehaviour
     //change patrol direction
     private void ChangePatrolDirection(PatrolData patrolData, PatrolDirection newPatrolDirection)
     {
+        
         if (directionChangeTime > patrolData.MoveDirectionDuration)
         {
             currentPatrolDirection = newPatrolDirection;
@@ -115,6 +179,7 @@ public class Module6 : MonoBehaviour
         }
     }
 
+    //movement player
     void Translate(Vector3 translation)
     {
         transform.position = transform.position + translation;
